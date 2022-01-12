012cc29e7aa588115958968c48aae7e12322c52c
chaz-interserv <chaz@interserv.com.au>
2022-01-12 23:45:49 +1100

    Added basic project files

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="app">
+    <TopBanner />
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+import HelloWorld from './components/HelloWorld.vue';
+import TopBanner from './components/TopBanner.vue';
+
+@Options({
+  components: {
+    HelloWorld,
+    TopBanner,
+  },
+})
+export default class App extends Vue {}
+</script>
+
+<style>
+.app {
+  /* Since Proxima Nova is a paid font I used googles free alternative */
+  font-family: 'Montserrat', sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+body {
+  margin: 0px;
+  background-color: #F5F8FF;
+}
+</style>
---
+++ src/components/TopBanner.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="top-banner">
+    <img alt="Vue logo" src="../assets/Plann_logo.png">
+
+    <button class="">
+    <span>
+      Home Base
+    </span>
+  </button>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+@Options({
+  props: {
+    msg: String
+  }
+})
+export default class TopBanner extends Vue {
+  msg!: string
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .top-banner {
+    background-color: white;
+    height: 60px;
+  }
+
+  button {
+    border-radius: 10px;
+    padding: 24px 60px;
+    background-color: #81C4EE;
+    border: none;
+    text-transform: uppercase;
+    cursor: pointer;
+    background-color: #fff;
+  border-color: #fafbff;
+  border-width: 1px;
+  color: #293147;
+  cursor: pointer;
+  justify-content: center;
+  padding-bottom: calc(.5em - 1px);
+  padding-left: 1.75em;
+  padding-right: 1.75em;
+  padding-top: calc(.5em - 1px);
+  text-align: center;
+  white-space: nowrap;
+}
+</style>
